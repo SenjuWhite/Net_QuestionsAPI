47e2155efe557ae565f8e1f3f4e8c1acdc0a4763
Nikita Skriaha <nikitaskraga6@gmail.com>
2024-08-30 13:07:35 +0300

    upd

---
+++ Questions_NET.DataAccess/Migrations/20240829212010_upd.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Questions_NET.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class upd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
